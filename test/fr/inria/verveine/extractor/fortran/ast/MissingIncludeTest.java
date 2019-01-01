3e5adb739866f0d8681e7de52dbe6efb6cea7627
NicolasAnquetil <nicolas.anquetil@inria.fr>
2019-01-01 12:17:49 +0100

    Tested parsing works with missing included file

---
+++ test/fr/inria/verveine/extractor/fortran/ast/MissingIncludeTest.java
@@ -0,0 +1,21 @@
+package fr.inria.verveine.extractor.fortran.ast;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+public class MissingIncludeTest extends AbstractASTTest {
+
+	public static final String SOURCE_CODE = "PROGRAM LE_PROGRAMME\n" +
+			"#include 'inexistant-include.file'\n" +
+			"END PROGRAM\n";
+
+	@Test
+	public void testProgram() {
+		parseCode(SOURCE_CODE);
+
+		assertEquals(1, ast.findAll(ASTMainProgramNode.class).size());
+
+	}
+
+}
