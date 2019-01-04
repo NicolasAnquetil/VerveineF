46a1e6c88a891e25dc968fe92b8f00f750c74f89
NicolasAnquetil <nicolas.anquetil@inria.fr>
2019-01-04 15:56:10 +0100

    Added test of #if / #else

--- test/fr/inria/verveine/extractor/fortran/parser/ast/preprocTest.java
+++ test/fr/inria/verveine/extractor/fortran/parser/ast/preprocTest.java
@@ -6,8 +6,8 @@ import org.junit.Test;
 
 public class preprocTest extends AbstractASTTest {
 
-	public static final String SOURCE_CODE = "module a_module\n" + 
-			//"!before #if\n" + 
+	public static final String SIMPLE_CONDITION =
+			"module a_module\n" + 
 			"#if MACRO1==1\n" + 
 			"#if MACRO2==1\n" + 
 			"integer :: VAR1\n" + 
@@ -17,24 +17,34 @@ public class preprocTest extends AbstractASTTest {
 			"integer :: VAR3\n" + 
 			"end module\n";
 
-	public static final String NOTEQUAL_CONDITION = "module a_module\n" + 
-			//"!before #if\n" + 
+	public static final String IFELSE_CONDITION =
+			"module a_module\n" + 
+			"#if MACRO==1\n" + 
+			"integer :: VAR1\n" + 
+			"#else\n" + 
+			"integer :: VAR2\n" + 
+			"integer :: VAR3\n" + 
+			"#endif\n" + 
+			"end module\n";
+
+	public static final String NOTEQUAL_CONDITION =
+			"module a_module\n" + 
 			"#if MACRO!=1\n" + 
 			"integer :: VAR1\n" + 
 			"#endif\n" + 
 			"integer :: VAR2\n" + 
 			"end module\n";
 
-	public static final String OR_CONDITION = "module a_module\n" + 
-			//"!before #if\n" + 
+	public static final String OR_CONDITION =
+			"module a_module\n" + 
 			"#if MACRO==0 || MACRO==1\n" + 
 			"integer :: VAR1\n" + 
 			"#endif\n" + 
 			"integer :: VAR2\n" + 
 			"end module\n";
 
-	public static final String AND_CONDITION = "module a_module\n" + 
-			//"!before #if\n" + 
+	public static final String AND_CONDITION =
+			"module a_module\n" + 
 			"#if MACRO1==1 || MACRO2==1\n" + 
 			"integer :: VAR1\n" + 
 			"#endif\n" + 
@@ -42,33 +52,48 @@ public class preprocTest extends AbstractASTTest {
 			"end module\n";
 
 	@Test
-	public void testMacrosNotDefined() {
-		parseCode(SOURCE_CODE);
+	public void testSimpleMacrosNotDefined() {
+		parseCode(SIMPLE_CONDITION);
 		assertEquals(1, ast.findAll(ASTTypeDeclarationStmtNode.class).size()); // VAR3
 	}
 
 	@Test
-	public void testWrongMacro1() {
-		parseCode(new String[] {"-DMACRO1=0"} , SOURCE_CODE);
+	public void testSimpleWrongMacro1() {
+		parseCode(new String[] {"-DMACRO1=0"} , SIMPLE_CONDITION);
 		assertEquals(1, ast.findAll(ASTTypeDeclarationStmtNode.class).size()); // VAR3
 	}
 
 	@Test
-	public void testMacro1NotMacro2() {
-		parseCode(new String[] {"-DMACRO1=1"} , SOURCE_CODE);
+	public void testSimpleMacro1NotMacro2() {
+		parseCode(new String[] {"-DMACRO1=1"} , SIMPLE_CONDITION);
 		assertEquals(2, ast.findAll(ASTTypeDeclarationStmtNode.class).size()); // VAR2, VAR3
 	}
 
 	@Test
-	public void testMacro1WrongMacro2() {
-		parseCode(new String[] {"-DMACRO1=1", "-DMACRO2=0"} , SOURCE_CODE);
+	public void testSimpleMacro1WrongMacro2() {
+		parseCode(new String[] {"-DMACRO1=1", "-DMACRO2=0"} , SIMPLE_CONDITION);
 		assertEquals(2, ast.findAll(ASTTypeDeclarationStmtNode.class).size()); // VAR2, VAR3
 	}
 
 	@Test
-	public void testMacro1AndMacro2() {
-		parseCode(new String[] {"-DMACRO1=1", "-DMACRO2=1"} , SOURCE_CODE);
+	public void testSimpleMacro1AndMacro2() {
+		parseCode(new String[] {"-DMACRO1=1", "-DMACRO2=1"} , SIMPLE_CONDITION);
 		assertEquals(3, ast.findAll(ASTTypeDeclarationStmtNode.class).size()); // VAR1, VAR2, VAR3
+	}
+
+	public void testIfelseUndefined() {
+		parseCode(new String[] {} , IFELSE_CONDITION);
+		assertEquals(2, ast.findAll(ASTTypeDeclarationStmtNode.class).size());
+	}
+
+	public void testIfelseTrue() {
+		parseCode(new String[] {"-DMACRO=1"} , IFELSE_CONDITION);
+		assertEquals(1, ast.findAll(ASTTypeDeclarationStmtNode.class).size());
+	}
+
+	public void testIfelseFalse() {
+		parseCode(new String[] {"-DMACRO=0"} , IFELSE_CONDITION);
+		assertEquals(2, ast.findAll(ASTTypeDeclarationStmtNode.class).size());
 	}
 
 	@Test
